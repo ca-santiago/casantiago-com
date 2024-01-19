b3c1741421b45950e6fd751347e6c5a753f1cea1
Carmen Santiago <greminoficial@gmail.com>
2024-01-18 20:40:22 -0600

    fix: scape quote

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ const BlogMainPage = () => {
 
     return (
         <div>
-            <h1>Welcome to Carmen's Blog</h1>
+            <h1>Welcome to Carmen%#39s Blog</h1>
             <p>Here is a list of the current blogs</p>
         </div>
     );
